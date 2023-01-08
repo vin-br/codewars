2d4b9874b39fe986e3e2b75bd9db7d2765cc0142
Vincent Boettcher <vincent.boettcher@runbox.no>
2023-01-08 15:24:03 +0100

    Black formatting exceptions

--- 6kyu/Stop gninnipS sdroW!.py
+++ 6kyu/Stop gninnipS sdroW!.py
@@ -15,6 +15,4 @@ spinWords( "This is another test" )=> returns "This is rehtona test"
 
 # Solution
 def spin_words(sentence):
-    return " ".join(
-        [word[::-1] if len(word) >= 5 else word for word in sentence.split()]
-    )
+    return " ".join([word[::-1] if len(word) >= 5 else word for word in sentence.split()])
